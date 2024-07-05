da2d4edd4472e3d975148c3c82fd657aab36346e
Nil Vaghela <nilrajsinh9@icloud.com>
2024-07-05 11:11:16 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,93 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trading.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class TradingSignal(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    buy = db.Column(db.String, nullable=False)
+    target = db.Column(db.String, nullable=False)
+    sl = db.Column(db.String, nullable=False)
+    status = db.Column(db.String, nullable=False, default='Pending')
+    indexName = db.Column(db.String, nullable=False)
+    IndexToken = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Trading Signal {self.name}>'
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+with app.app_context():
+
+    db.create_all()
+
+
+@app.route('/api/trading_signal', methods=['POST'])
+def add_trading_signal():
+    data = request.json
+    new_signal = TradingSignal(
+        name=data['name'],
+        buy=data['buy'],
+        target=data['target'],
+        sl=data['sl'],
+        status=data['Status'],
+        indexName=data['indexName'],
+        IndexToken=data['IndexToken']
+    )
+    db.session.add(new_signal)
+    db.session.commit()
+    return jsonify({'message': 'Trading signal added', 'id': new_signal.id}), 201
+
+@app.route('/api/user', methods=['POST'])
+def add_user():
+    data = request.json
+    new_user = User(
+        username=data['username'],
+        email=data['email']
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'User added', 'id': new_user.id}), 201
+
+
+@app.route('/api/trading_signals', methods=['GET'])
+def get_trading_signals():
+    signals = TradingSignal.query.all()
+    results = [{
+        'id': signal.id,
+        'name': signal.name,
+        'buy': signal.buy,
+        'target': signal.target,
+        'sl': signal.sl,
+        'status': signal.status,
+        'indexName': signal.indexName,
+        'IndexToken': signal.IndexToken
+    } for signal in signals]
+    return jsonify(results), 200
+
+@app.route('/api/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    results = [{
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+        'created_at': user.created_at.isoformat()
+    } for user in users]
+    return jsonify(results), 200
+
+if __name__ == '__main__':
+    
+    app.run(debug=True)
